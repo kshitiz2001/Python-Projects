617c43b65e7f5cec47220029c58d124d5fdf0354
Kshitiz Kumar Singh <kshitiz.singh_cs20@gla.ac.in>
2021-06-10 21:31:59 +0530

    Add files via upload

---
+++ character_occurence.py
@@ -0,0 +1,10 @@
+n = input("Enter Your Word Or Sentence : ")
+def count_characters(s):
+    count = {}
+    for i in s:
+        if i in count:
+            count[i] += 1
+        else:
+            count[i] = 1
+    print(count)
+print(count_characters(n))
