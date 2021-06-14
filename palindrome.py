fc711a2dee5446f2409c680b46d395ee13b68d38
Kshitiz Kumar Singh <kshitiz.singh_cs20@gla.ac.in>
2021-06-14 10:39:20 +0530

    Palindrome Identifier

---
+++ palindrome.py
@@ -0,0 +1,15 @@
+sentence = input("Enter a sentence : ")
+def palindrome(sentence):
+    for i in (",.'?/><}{{}}'"):
+        sentence = sentence.replace(i, "")
+    palindrome = []
+    words = sentence.split(' ')
+    for word in words:
+        word = word.lower()
+        if word == word[::-1]:
+            palindrome.append(word)
+    return palindrome
+
+print(palindrome(sentence))
+
+# by kshitiz
