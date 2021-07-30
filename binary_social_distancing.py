0a9063ba48c3ddbd9d7b4dc3fc99355e00c83efd
Kshitiz Kumar Singh <kshitiz.singh_cs20@gla.ac.in>
2021-07-30 16:16:33 +0530

    Binary Social Distancing

---
+++ binary_social_distancing.py
@@ -0,0 +1,14 @@
+n= int(input())
+if(n==0):
+    print("0")
+else:
+    a=[0 for i in range(n)]
+    b=[0 for i in range(n)]
+    a[0] = b[0] = 1
+    for i in range(1,n):
+        a[i] = a[i-1] + b[i-1]
+        b[i] = a[i-1]
+
+    print(a[n-1] + b[n-1]-1)
+    
+    # by kshitiz
